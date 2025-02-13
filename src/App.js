4fa1c0cdb577708d982fb149444d0f4f8f9e7b5f
vikhyath04 <vikhyathhegde19@gmail.com>
2025-02-13 23:31:01 +0530

    TO-DO LIST

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper'; // Use named import
+
+
+function App() {
+  return (
+    <div className="App">
+     <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
